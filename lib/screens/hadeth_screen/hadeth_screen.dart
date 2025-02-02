1d4948bc97c295bc4e6a768833ec434d8a5704b4
hasan-hendawi <eng.hasan.hendawi@gmail.com>
2025-02-02 15:02:07 +0200

    add hadteh widget

---
+++ lib/screens/hadeth_screen/hadeth_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HadethScreen extends StatelessWidget {
+  static const String HadethScreenRoutePage = "hadeth_screen";
+
+   HadethScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(color:
+      Colors.green,);
+  }
+}
--- lib/screens/home_screen/widgets/hadeth_widget.dart
+++ lib/screens/home_screen/widgets/hadeth_widget.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/screens/hadeth_screen/hadeth_screen.dart';
 
 class HadethWidget extends StatelessWidget {
-  const HadethWidget({super.key});
-
+   HadethWidget({super.key});
+List<HadethDetails> ahadethDetails=[];
   @override
   Widget build(BuildContext context) {
+    readFile();
     return  Container(
-      color: Colors.amberAccent,
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(flex: 1,
+              child: Image.asset("assets/images/hadeth_header.png")),
+          Divider(thickness: 3,color: Theme.of(context).primaryColor,),
+          Text("Hadeth Name",style: TextStyle(fontSize: 24),),
+          Divider(thickness: 3,color: Theme.of(context).primaryColor,),
+          Expanded(flex: 3,
+            child: ListView.separated(itemBuilder: (context,index){
+              return InkWell(
+                  onTap: (){
+                    Navigator.of(context).pushNamed(HadethScreen.HadethScreenRoutePage,);
+                  },
+                  child: Text(ahadethDetails[index].hadethTitle,style: TextStyle(fontSize: 25),textAlign: TextAlign.center,));
+            },
+              separatorBuilder: (context,i){
+                return Divider(thickness: 2,color:Theme.of(context).primaryColor,indent: 16, endIndent: 16,height: 20,);
+              }
+              ,itemCount:ahadethDetails.length,
+            ),
+          )
+        ],
+      ),
     );
   }
+  void readFile()async{
+    String content=await rootBundle.loadString("assets/hadeth_content/ahadeth.txt");
+    // print(content);
+   List<String> ahadeth= content.trim().split("#");
+   for(int i =0 ;i<ahadeth.length;i++){
+     List<String> hadeth = ahadeth[i].trim().split("\n");
+
+    String hadethTitle= hadeth[0].trim(); //الحديث الاول
+     hadeth.removeAt(0);
+
+    String hadethContent = hadeth.join("\n");
+     ahadethDetails.add(HadethDetails(hadethTitle: hadethTitle, hadethContent: hadethContent));
+   }
+
+    print(ahadethDetails[1].hadethTitle);
+
+  }
+}
+class HadethDetails{
+  String hadethTitle;
+  String hadethContent;
+  HadethDetails({required this.hadethTitle, required this.hadethContent});
 }
